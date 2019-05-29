f5a59426e02b24e2a493d59e3363b97471b0fa23
sami <sami-zhioua>
2019-05-29 11:35:00 +0300

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -64,7 +64,8 @@ app.get('/bad',
 app.use(express.static(__dirname + '/public'));
 
 
+const port = process.env.PORT || 3000;
 
-
-
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
